94c99515d9b4b2d0a7132da041ade8fbe0340967
Fivetr <81582626+Fivetr@users.noreply.github.com>
2023-03-18 19:16:52 -0500

    Update SQL_script.sql
    tutor_id type -> varchar(2550

--- backend/SQL_script.sql
+++ backend/SQL_script.sql
@@ -5,14 +5,14 @@ CREATE TABLE Student(
     phone VARCHAR(30),
     email VARCHAR(255),
     password VARCHAR(255),
-	minutes_tutored INTEGER,
-	tutor_id INTEGER NOT NULL,
+    minutes_tutored INTEGER,
+    tutor_id VARCHAR(255)L,
     index is_tutor(tutor_id),
     PRIMARY KEY(netid)
 );
 
 CREATE TABLE Tutor(
-    tutor_id INTEGER AUTO_INCREMENT,
+    tutor_id VARCHAR(255),
     about_me VARCHAR(5000),
     profile_pic BLOB,
     PRIMARY KEY(tutor_id),
@@ -20,7 +20,7 @@ CREATE TABLE Tutor(
 );
 
 CREATE TABLE Availability(
-	tutor_id INTEGER,
+    tutor_id VARCHAR(255),
     start_time DATETIME,
     end_time DATETIME,
     PRIMARY KEY (tutor_id, start_time, end_time),
@@ -28,7 +28,7 @@ CREATE TABLE Availability(
 );
 
 CREATE TABLE Favorites(
-	tutor_id INTEGER,
+	tutor_id VARCHAR(255),
     student_id VARCHAR(255),
 	PRIMARY KEY(student_id, tutor_id),
     FOREIGN KEY(student_id) REFERENCES Student(netid),
@@ -36,19 +36,19 @@ CREATE TABLE Favorites(
 );
 
 CREATE TABLE Subjects(
-	tutor_id INTEGER,
+	tutor_id VARCHAR(255),
 	subject VARCHAR(255),
     PRIMARY KEY (tutor_id, subject),
     FOREIGN KEY(tutor_id) REFERENCES Tutor(tutor_id)
 );
 
 CREATE TABLE Appointments(
-	tutor_id INTEGER,
+	tutor_id VARCHAR(255),
     student_id VARCHAR(255),
 	start_time DATETIME,
     end_time DATETIME,
 	subject VARCHAR(255),
-    PRIMARY KEY (tutor_id, student_id),
+    PRIMARY KEY (tutor_id, student_id, start_time, end_time),
     FOREIGN KEY(tutor_id) REFERENCES Tutor(tutor_id),
     FOREIGN KEY(student_id) REFERENCES Student(netid)
 );
